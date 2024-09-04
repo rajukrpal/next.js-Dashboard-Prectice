216b2f1cacd102a45ae4dbbb9ff0eae8368a4888
rajukrpal <rajukumarpal95@gmail.com>
2024-09-04 16:25:01 +0530

    chart-js problem

--- src/components/ChartAverage.jsx
+++ src/components/ChartAverage.jsx
@@ -1,86 +1,103 @@
 
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import Chart from "chart.js";
 
 const ChartAverage = () => {
+    const chartRef = useRef();
+    const chartInstanceRef = useRef(null);
 
-    React.useEffect(() => {
-        let config = {
-            type: "bar",
-            data: {
-                labels: [
-                    "Monday",
-                    "Tuesday",
-                    "Wednesday",
-                    "Thursday",
-                    "Friday",
-                    "Saturday",
-                    "Sunday",
-                ],
-                datasets: [
-                    {
-                        label: new Date().getFullYear(),
-                        backgroundColor: "#0095FF",
-                        borderColor: "#4a5568",
-                        data: [30, 78, 56, 34, 100, 45, 13],
-                        barThickness: 20, // Adjust this value for bar width
-                    },
-                    {
-                        label: new Date().getFullYear() - 1,
-                        backgroundColor: "#00E096",
-                        borderColor: "#3182ce",
-                        data: [27, 68, 86, 74, 10, 4, 87],
-                        barThickness: 20, // Adjust this value for bar width
-                    },
-                ],
-            },
-            options: {
-                maintainAspectRatio: false,
-                responsive: true,
-                title: {
-                    display: false,
-                    text: "Orders Chart",
-                },
-                tooltips: {
-                    mode: "index",
-                    intersect: false,
-                },
-                hover: {
-                    mode: "nearest",
-                    intersect: true,
-                },
-                legend: {
-                    labels: {
-                        fontColor: "rgba(0,0,0,.4)",
-                    },
-                    align: "end",
-                    position: "bottom",
-                },
-                scales: {
-                    x: {
-                        barPercentage: 40, 
-                        categoryPercentage: 20, 
-                        grid: {
-                            display: false,
+    useEffect(() => {
+        if (chartRef && chartRef.current) {
+            const myChartRef = chartRef.current.getContext("2d");
+            // Destroy previous chart instance
+            if (chartInstanceRef.current !== null) {
+                chartInstanceRef.current.destroy();
+            }
+            chartInstanceRef.current = new Chart(myChartRef, {
+                type: "bar",
+                data: {
+                    labels: [
+                        "Monday",
+                        "Tuesday",
+                        "Wednesday",
+                        "Thursday",
+                        "Friday",
+                        "Saturday",
+                        "Sunday",
+                    ],
+                    datasets: [
+                        {
+                            label: new Date().getFullYear(),
+                            backgroundColor: "#0095FF",
+                            borderColor: "#4a5568",
+                            data: [30, 78, 56, 34, 100, 45, 13],
+                            barThickness: 20, // Adjust this value for bar width
                         },
-                        ticks: {
-                            display: false,
+                        {
+                            label: new Date().getFullYear() - 1,
+                            backgroundColor: "#00E096",
+                            borderColor: "#3182ce",
+                            data: [27, 68, 86, 74, 10, 4, 87],
+                            barThickness: 20, // Adjust this value for bar width
                         },
+                    ],
+                },
+                options: {
+                    maintainAspectRatio: false,
+                    responsive: true,
+                    title: {
+                        display: false,
+                        text: "Orders Chart",
                     },
-                    y: {
-                        grid: {
-                            borderDash: [2],
-                            drawBorder: false,
-                            borderDashOffset: [2],
-                            color: "rgba(33, 37, 41, 0.2)",
-                            zeroLineColor: "rgba(33, 37, 41, 0.15)",
-                            zeroLineBorderDash: [2],
-                            zeroLineBorderDashOffset: [2],
+                    tooltips: {
+                        mode: "index",
+                        intersect: false,
+                    },
+                    hover: {
+                        mode: "nearest",
+                        intersect: true,
+                    },
+                    legend: {
+                        labels: {
+                            fontColor: "rgba(0,0,0,.4)",
+                        },
+                        align: "end",
+                        position: "bottom",
+                    },
+                    scales: {
+                        x: {
+                            barPercentage: 40,
+                            categoryPercentage: 20,
+                            grid: {
+                                display: false,
+                            },
+                            ticks: {
+                                display: false,
+                            },
+                        },
+                        y: {
+                            grid: {
+                                borderDash: [2],
+                                drawBorder: false,
+                                borderDashOffset: [2],
+                                color: "rgba(33, 37, 41, 0.2)",
+                                zeroLineColor: "rgba(33, 37, 41, 0.15)",
+                                zeroLineBorderDash: [2],
+                                zeroLineBorderDashOffset: [2],
+                            },
                         },
                     },
                 },
-            },
-        };
+
+            });
+            // Clean up function
+            return () => {
+                // Destroy chart instance when component unmounts
+                if (chartInstanceRef.current !== null) {
+                    chartInstanceRef.current.destroy();
+                }
+            };
+        }
         let ctx = document.getElementById("bar-chart").getContext("2d");
         new Chart(ctx, config);
     }, []);
@@ -103,7 +120,7 @@ const ChartAverage = () => {
                 <div className="p-4 flex-auto">
                     {/* Chart */}
                     <div className="relative h-350-px h-72">
-                        <canvas id="bar-chart"></canvas>
+                        <canvas ref={chartRef} id="bar-chart"></canvas>
                     </div>
                 </div>
             </div>
--- src/components/ChartLine.jsx
+++ src/components/ChartLine.jsx
@@ -1,140 +1,22 @@
 
-// import React, { useEffect } from 'react';
-// import Chart from "chart.js";
 
-// const ChartLine = () => {
-
-//     useEffect(() => {
-//         var config = {
-//             type: "line",
-//             data: {
-//                 labels: [
-//                     "January",
-//                     "February",
-//                     "March",
-//                     "April",
-//                     "May",
-//                     "June",
-//                     "July",
-        
-//                 ],
-//                 datasets: [
-//                     {
-//                         label: new Date().getFullYear(),
-//                         backgroundColor: "#3182ce",
-//                         borderColor: "red",
-//                         data: [65, 78, 66, 24, 56, 67, 75],
-//                         fill: false,
-//                     },
-//                     {
-//                         label: new Date().getFullYear(),
-//                         backgroundColor: "#3182ce",
-//                         borderColor: "#3CD856",
-//                         data: [65, 88, 70, 70, 86, 57, 65],
-//                         fill: false,
-//                     },
-//                     {
-//                         label: new Date().getFullYear() - 1,
-//                         fill: false,
-//                         backgroundColor: "#edf2f7",
-//                         borderColor: "#A700FF",
-//                         data: [60, 68, 86, 74, 56, 60, 87],
-//                     },
-//                 ],
-//             },
-//             options: {
-//                 maintainAspectRatio: false,
-//                 responsive: true,
-//                 title: {
-//                     display: false,
-//                 },
-//                 legend: {
-//                     labels: {
-//                         fontColor: "#000", 
-//                     },
-//                     align: "end",
-//                     position: "bottom",
-//                 },
-//                 tooltips: {
-//                     mode: "index",
-//                     intersect: false,
-//                     backgroundColor: "rgba(0, 0, 0, 0.8)",
-//                     titleFontColor: "#000",
-//                     bodyFontColor: "#000", 
-//                 },
-//                 hover: {
-//                     mode: "nearest",
-//                     intersect: true,
-//                 },
-//                 scales: {
-//                     xAxes: [
-//                         {
-//                             ticks: {
-//                                 fontColor: "#000", 
-//                             },
-//                             display: true,
-//                             scaleLabel: {
-//                                 display: false,
-//                             },
-//                             gridLines: {
-//                                 display: false,
-//                                 color: "rgba(255, 255, 255, 0.7)", 
-//                             },
-//                         },
-//                     ],
-//                     yAxes: [
-//                         {
-//                             ticks: {
-//                                 fontColor: "#000", 
-//                             },
-//                             display: true,
-//                             scaleLabel: {
-//                                 display: false,
-//                             },
-//                             gridLines: {
-//                                 color: "rgba(255, 255, 255, 0.9)", 
-//                             },
-//                         },
-//                     ],
-//                 },
-//             },
-//         };
-//         var ctx = document.getElementById("line-chart").getContext("2d");
-//         window.myLine = new Chart(ctx, config);
-//     }, []);
-
-//     return (
-//         <>
-//             <div className="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded bg-blueGray-700 text-black h-full">
-//                 <div className="rounded-t mb-0 px-4 py-3 bg-transparent">
-//                     <div className="flex flex-wrap items-center">
-//                         <div className="relative w-full max-w-full flex-grow flex-1">
-//                             <h2 className="text-xl font-semibold">Visitor Insights</h2>
-//                         </div>
-//                     </div>
-//                 </div>
-//                 <div className=" flex-auto h-full">
-//                     {/* Chart */}
-//                     <div className="relative h-500-px h-64 "> 
-//                         <canvas id="line-chart"></canvas>
-//                     </div>
-//                 </div>
-//             </div>
-//         </>
-//     );
-// }
-
-// export default ChartLine;
-
-
-import React, { useEffect } from 'react';
-import Chart from "chart.js";
+import React, { useEffect, useRef } from 'react';
+import Chart from "chart.js"; 
 
 const ChartLine = () => {
+    const chartRef = useRef();
+    const chartInstanceRef = useRef(null);
 
     useEffect(() => {
-        if (typeof window !== 'undefined') {
-            var config = {
+        if (chartRef && chartRef.current) {
+            const myChartRef = chartRef.current.getContext("2d");
+
+            // Destroy previous chart instance
+            if (chartInstanceRef.current !== null) {
+                chartInstanceRef.current.destroy();
+            }
+            chartInstanceRef.current = new Chart(myChartRef, {
+
                 type: "line",
                 data: {
                     labels: [
@@ -178,7 +60,7 @@ const ChartLine = () => {
                     },
                     legend: {
                         labels: {
-                            fontColor: "#000", 
+                            fontColor: "#000",
                         },
                         align: "end",
                         position: "bottom",
@@ -188,7 +70,7 @@ const ChartLine = () => {
                         intersect: false,
                         backgroundColor: "rgba(0, 0, 0, 0.8)",
                         titleFontColor: "#000",
-                        bodyFontColor: "#000", 
+                        bodyFontColor: "#000",
                     },
                     hover: {
                         mode: "nearest",
@@ -198,7 +80,7 @@ const ChartLine = () => {
                         xAxes: [
                             {
                                 ticks: {
-                                    fontColor: "#000", 
+                                    fontColor: "#000",
                                 },
                                 display: true,
                                 scaleLabel: {
@@ -206,29 +88,34 @@ const ChartLine = () => {
                                 },
                                 gridLines: {
                                     display: false,
-                                    color: "rgba(255, 255, 255, 0.7)", 
+                                    color: "rgba(255, 255, 255, 0.7)",
                                 },
                             },
                         ],
                         yAxes: [
                             {
                                 ticks: {
-                                    fontColor: "#000", 
+                                    fontColor: "#000",
                                 },
                                 display: true,
                                 scaleLabel: {
                                     display: false,
                                 },
                                 gridLines: {
-                                    color: "rgba(255, 255, 255, 0.9)", 
+                                    color: "rgba(255, 255, 255, 0.9)",
                                 },
                             },
                         ],
                     },
                 },
+            })
+            // Clean up function
+            return () => {
+                // Destroy chart instance when component unmounts
+                if (chartInstanceRef.current !== null) {
+                    chartInstanceRef.current.destroy();
+                }
             };
-            var ctx = document.getElementById("line-chart").getContext("2d");
-            window.myLine = new Chart(ctx, config);
         }
     }, []);
 
@@ -244,8 +131,8 @@ const ChartLine = () => {
                 </div>
                 <div className="flex-auto h-full">
                     {/* Chart */}
-                    <div className="relative h-500-px h-64"> 
-                        <canvas id="line-chart"></canvas>
+                    <div className="relative h-500-px h-64">
+                        <canvas ref={chartRef} id="line-chart"></canvas>
                     </div>
                 </div>
             </div>
